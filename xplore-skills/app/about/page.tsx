35ce96b3435901d0f0b07791a30f3bba0f883050
Abdul Rehman <92779012+Abdulrehman512@users.noreply.github.com>
2023-11-25 22:49:29 +0500

    Add files via upload

---
+++ xplore-skills/app/about/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react';
+import Head from 'next/head';
+
+export default function About() {
+    return (
+<section className="about-us">
+        <div className="row">
+            
+            <div className="about-col">
+                <h1>We are the world's largest University</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore cumque in accusantium molestias maiores fuga vitae eos, ducimus officiis repudiandae voluptatem error. Laboriosam, numquam blanditiis aspernatur, nobis tempora consequatur id aliquam asperiores voluptatum iste animi voluptatem fugiat totam excepturi dolor.</p>
+                <a href="#" className="hero_btn btn">EXPLORE NOW</a>
+            </div>
+            <div className="about-col">
+                <img src="img/about.png" alt=""/>    
+            </div>
+        </div>
+    </section>
+    )
+  }
